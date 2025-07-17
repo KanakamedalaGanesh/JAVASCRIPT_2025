375190398e573ad1c89c71d2bbbd1dce92d6f511
Ganesh <ganeshkanakamedala@gmail.com>
2025-07-17 08:31:11 +0530

    Different array methods and its uses.

---
+++ 10.ARRAY_METHODS/Array_Methods.js
@@ -0,0 +1,13 @@
+let arr1= ["Hello world","Ganesh"]
+console.log(arr1.at(8));
+console.log(arr1.at(-1));
+console.log(arr1.push("How are you"));
+console.log(arr1);
+console.log(arr1.pop());
+console.log(arr1);
+console.log(arr1.unshift("Bat","Ball"));
+console.log(arr1);
+console.log(arr1.shift());
+console.log(arr1);
+console.log(arr1.splice(1,2));
+console.log(arr1);
