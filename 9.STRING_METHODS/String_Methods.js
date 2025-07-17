c7ece75fa323f715b7698a749db961ef17e3b868
Ganesh <ganeshkanakamedala@gmail.com>
2025-07-17 08:29:17 +0530

    String methods examples ans uses.

---
+++ 9.STRING_METHODS/String_Methods.js
@@ -0,0 +1,13 @@
+ var str = "Hello, World!";
+        console.log(str.length); // Output: 13
+        console.log(str.indexOf("World")); // Output: 7
+        console.log(str.charAt(1)); // Output: e
+        console.log(str.charCodeAt(9)); // Output: 114
+        console.log(str.concat(" This is Ganesh")); // Output: Hello, World This is Ganesh
+        console.log(str.endsWith("!")); // Output: true
+        console.log(str.startsWith("h")); // Output: false
+        console.log(str.includes("l",3)); // Output: true
+        console.log(str.indexOf(" ")); // Output: 6
+        console.log(str.toUpperCase()); // Output: HELLO, WORLD 
+        console.log(str.toLowerCase()); // Output: hello, world
+        
